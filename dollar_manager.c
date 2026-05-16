b25b75793c811d74721731afc9a90c40bfcfe933
thedixon11 <joaquimv1@hotmail.com>
2026-05-16 19:31:43 +0200

    WIP : update dollar manager et create env var manager

--- dollar_manager.c
+++ dollar_manager.c
@@ -1,4 +1,4 @@
-char  *dollar_manager(char *str, int *i)
+char  *dollar_manager(t_data *data, char *str, int *i)
 {
   char  *result;
   //si ce qui suit le $ est un digit ou un quote (single ou double), alors
@@ -13,24 +13,25 @@ char  *dollar_manager(char *str, int *i)
 //de sortie la commande d'avant
   else if (str[*i + 1] == '?')
   {
-    result = find_exit_error;
+    result = find_exit_error();
     return (result);
   }
 
 //si ce qui suit le $ est soit une lettre soit un underscore,
-// alors on doit checker si c'est une env var
-  else if (ft_isalpha(str[*i + 1]) == 0 || str[*i + 1] == '_')
+// alors on doit checker si c'est une env var et si oui expandre
+  else if (ft_isalpha(str[*i + 1]) == 1 || str[*i + 1] == '_')
   {
-    result = env_var_checker();
+    result = env_var_manager(data, str, i);
     return (result);
   }
 
 // si ce qui suit le $ est autre chose,
-// on considere le $ et le char qui le suit comme normaux (pas cas special)
+// on considere le $ et le char qui le suit comme normaux (pas un cas special)
   else
   {
-
+    result = ft_calloc(3, sizeof(char));
+    result[0] = '$';
+    result[1] = str[*i + 1];
+    return (result);
   }
-    
-
 }
---
+++ env_var_manager.c
@@ -0,0 +1,63 @@
+
+char  *env_var_checker(t_env *env, char *var_env)
+{
+  t_env *current;
+  int len;
+  char  *xpansion_result;
+
+  //ici, on va comparer notre var_env et voir si elle existe dans l'environnement
+  //il est important de comparer la len de var_env + 1 (inclus le /0) pour etre sur
+  //qu'il n'y ait rien apres.
+  //a noter que l'environnement est stocker dans une double liste chainee, contentant
+  //pour chaque node : name, content, prev et next
+  len = ft_strlen(var_env) + 1;
+  current = env;
+  while (current != NULL)
+  {
+    if (ft_strncpy(var_env, current->name, len) == 0)
+    {
+      xpansion_result = ft_strdup(current->content);
+      return (xpansion_result);
+    }
+    current = current->next;
+  }
+  return (NULL);
+}
+
+char  *extract_env_var(char *str, int start)
+{
+  int end;
+  int len;
+  char  *var_env;
+
+  //pour extraire la env_var, on doit determiner la fin de celle-ci
+  //toute env_var ne peut conternir que des lettres, digits et underscore
+  //elle ne peut pas commencer par un digit
+  start++;
+  end = start;
+  len = 0;
+  while(ft_isalnum(str[end]) == 1 || str[end] == '_')
+    end++;
+  
+  //une fois la fin de la var_env trouvee, on va l'extraire de la str
+  //grace a substr. on a besoin pour ca de la len de la var_env
+  len = end - start;
+  var_env = ft_substr(str, start, len);
+  return (var_env);
+}
+
+char  *env_var_manager(t_data *data, char *str, int *i)
+{
+  char  *xpansion_result;
+  char  *var_env;
+
+  //pour commencer, il faut qu'on extraie la var env potentielle
+  //contenue dans la string
+  var_env = extract_env_var(str, start);
+
+  //une fois la env_var extraite, on va checker si elle existe bien
+  //dans notre environnement, et si oui, on va l'expand
+  xpansion_result = env_var_checker(data->env, var_env);
+
+  return (xpansion_result);
+}
