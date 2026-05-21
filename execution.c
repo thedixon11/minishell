414864dec71ceeaacc0963251da5c19c8a43aef8
Joaquim Vasconcelos <jvasconc@c2r1s6.42lausanne.ch>
2026-05-21 14:50:36 +0200

    WIP : execution in progress

--- execution.c
+++ execution.c
@@ -2,21 +2,28 @@
 
 //TODO: pas du tout bien structurer, faut encore BEAUCOUP travailler dessus !
 
-int patch_input()
+int patch_input(t_data *data, t_line *line_cmd, t_env *env)
+{
+  
+}
 
-void  child_process(t_data *data, t_line *line_cmd, t_env *env)
+void  child_process(t_data *data, t_line *line_cmd, t_env *env, int current_cmd)
 {
   int input;
   int output;
-  t_list  *current;
+  t_line  *current;
 
   input = 0;
   output = 0;
   current = line_cmd;
-  while (current != NULL)
+  while (current->cmd_nb == current_cmd) 
   {
-
+    if (current->type == T_INPUT)
+      patch_input(data, line_cmd, env);
+    if (current->type == T_OUTPUT)
+      patch_output(data, line_cmd, env)
   }
+
 
 }
 
@@ -54,7 +61,7 @@ void	execute_cmds(t_data *data, t_line *line_cmd, t_env *env)
 		if (pid == -1)
 			errors_exit(data, FORK_ERR, 0, 0);
 		if (pid == 0)
-			child_process(data);
+			child_process(data, line_cmd, env, current_cmd);
 		if (pid > 0)
 			parent_process(data, current_cmd);
 	}
