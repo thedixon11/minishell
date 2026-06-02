7a8edeb3932de579713fa9bcaba4669a04c75d45
Joaquim Vasconcelos <jvasconc@c1r5s5.42lausanne.ch>
2026-06-02 11:22:19 +0200

    moved utils functions in quote manager, deleted utils file, updated header with all functions

--- minishell_xpansion.h
+++ minishell_xpansion.h
@@ -15,16 +15,26 @@ typedef enum e_bool
   B_TRUE
 } t_bool;
 
-//dollar manager function
+//value manager functions
+char  **val_manager(char *content);
+char  *expand_off_quote(t_data *data, char *content);
+char  *expand_in_quote(t_data *data, char *row);
+char  *go_until_quote(char *content, int *start);
+char  *go_until_dollar(char *content, int *start);
+
+//dollar manager functions
 char  *dollar_manager(t_data *data, char *str, int *i, t_quote q_mode);
 
-//environment variable manager function
+//environment variable manager functions
 char  *env_var_manager(t_data *data, char *str, int *start);
+char  *extract_env_var(char *str, int *i);
+char  *env_var_checker(t_env *env, char *var_env);
 
-//quote_manager
+//quote_manager functions
 char  *quote_manager(t_data *data, char *content, int *i_content, char quote);
-
-//xpansion_utils
+char  *extract_quote(char *value, int *i_value, char quote);
+char  *quote_expansion(t_data *data, char *inside_quote);
 char  *second_block_not_dollar_or_quote(char *str, int *i, t_quote q_mode);
+t_bool is_it_dollar_or_quote(char c, t_quote q_mode);
 
 #endif
