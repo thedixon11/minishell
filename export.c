46b41c732ead07243f1565dd653f04b5ff80faba
Joaquim Vasconcelos <jvasconc@c3r4s4.42lausanne.ch>
2026-07-16 15:11:54 +0200

    clest la merde

--- export.c
+++ export.c
@@ -1,16 +1,25 @@
+void	manage_value_export(t_data *data, t_line *cmd, char *value)
+{
+	int		y;
+
+	y = 1;
+}
+
+
+void	add_to_env(t_data *data)
+{
+	if (check_if_already_exists(data) == B_TRUE)
+}
 
 int	ft_export(t_data *data, t_line *cmd)
 {
 	int	y;
 
-	y = 0;
+	y = 1;
 	
-	if (data->while
-		
+
 	while (cmd->content_xpand[y] != NULL)
-		y++;
+		manage_value_export(data, cmd->content_xpand[y]);
 	if (y == 1)
 		print_env_sorted(data);
-	else
-		execute_export(data);
 }
