e07fa30b2dba6c6755acf28efc6aee76637dcf44
ageoffro <ageoffro@student.42lausanne.ch>
2026-08-04 20:10:31 +0200

    start env

--- src_parsing/create_env.c
+++ src_parsing/create_env.c
@@ -2,11 +2,45 @@
 
 t_env	*create_env(char **envp)
 {
-	int	x;
-	t_env env;
+	int		x;
+	t_env	*head;
+	t_env	*node;
 
+	x = 0;
 	while (envp[x] != NULL)
 	{
-		
+
 	}
+}
+
+void	add_node_env(t_env *head)
+{
+	t_env	*temp;
+	t_env	*current;
+
+	if (head == NULL)
+		head = current;
+	else
+	{
+		temp = head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		current->next = NULL;
+		current->prev = temp;
+		temp->next = current;
+	}
+}
+
+t_env	*new_node_env(char *line)
+{
+	t_env	*node;
+	char	*equal;
+
+	node = calloc(1, sizeof(t_env));
+	if (!node)
+		return (NULL);
+	equal = ft_strchr(line, '=');
+	node->name = ft_strndup(line, equal - line);
+	node->content = strdup(equal + 1);
+	return (node);
 }
