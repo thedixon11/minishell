21cec788fea34a9349c5e0edb490a0fda2fd3c0c
Joaquim Vasconcelos <jvasconc@c2r2s6.42lausanne.ch>
2026-08-09 16:16:20 +0200

    WIP : I got issues with unset, i'm losing my env head, will create a new branch to try to start my data struct from the top of alexs code

--- src_builtin/ft_unset.c
+++ src_builtin/ft_unset.c
@@ -1,5 +1,26 @@
 #include "../minishell_general.h"
 
+void	free_env_node(t_env *to_delete)
+{
+		ft_free((void **)&to_delete->name);
+		ft_free((void **)&to_delete->content);
+		ft_free((void **)&to_delete);
+}
+
+void	delete_node(t_env *to_delete)
+{
+	t_env	*before;
+	t_env	*after;
+
+	before = to_delete->prev;
+	after = to_delete->next;
+	if (after != NULL)
+		after->prev = before;
+	if (before != NULL)
+		before->next = after;
+	free_env_node(to_delete);
+}
+
 int	remove_the_env_var(t_data *data, char *env_var_to_rm)
 {
 	t_env	*current;
@@ -13,13 +34,11 @@ int	remove_the_env_var(t_data *data, char *env_var_to_rm)
 	{
 		if (ft_strncmp(env_var_to_rm, current->name, size) == 0)
 		{
+			if (current->prev == NULL)
+				data->env = data->env->next;
 			to_delete = current;
-			current->prev->next = current->next;
-			current->next->prev = current->prev;
 			current = current->next;
-			ft_free((void **)&to_delete->name);
-			ft_free((void **)&to_delete->content);
-			ft_free((void **)&to_delete);
+			delete_node(to_delete);
 		}
 		else
 			current = current->next;
