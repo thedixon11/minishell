416941e99fe3c022f99096f7e1492fee6a173a29
Joaquim Vasconcelos <jvasconc@c2r7s2.42lausanne.ch>
2026-08-26 22:03:00 +0200

    fixed the echo -n cases, be strong ma boy monday will be the day

--- src_builtin/ft_echo.c
+++ src_builtin/ft_echo.c
@@ -12,23 +12,24 @@
 
 #include "../minishell_general.h"
 
-void	echo_no_args(t_data *data)
+int	echo_no_args(t_data *data)
 {
+	if (write(STDOUT_FILENO, "\n", 1) == -1)
+		return (error_int(data, I_WRITE, strerror(errno), 1));
 	if (data->do_i_exit == B_TRUE)
 	{
 		free_env(&data->env);
 		free_and_close_life(data);
 		exit(0);
 	}
+	return (0);
 }
 
-t_bool	skip_option_n(t_data *data, char **args_cmd, int *y)
+t_bool	skip_option_n(char **args_cmd, int *y)
 {
 	int	x;
 
 	x = 2;
-	if (args_cmd[1] == NULL)
-		echo_no_args(data);
 	if (args_cmd[*y][0] != '-' || args_cmd[*y][1] != 'n')
 		return (B_TRUE);
 	while (args_cmd[*y][x] != 0 && args_cmd[*y][x] == 'n')
@@ -41,13 +42,28 @@ t_bool	skip_option_n(t_data *data, char **args_cmd, int *y)
 		x = 2;
 		if (args_cmd[*y][0] != '-' || args_cmd[*y][1] != 'n')
 			return (B_FALSE);
-		while (args_cmd[*y][x] != 0 || args_cmd[*y][x] == 'n')
+		while (args_cmd[*y][x] != 0 && args_cmd[*y][x] == 'n')
 			x++;
 		if (args_cmd[*y][x] != 0)
 			return (B_FALSE);
 		(*y)++;
 	}
 	return (B_FALSE);
+}
+
+int	print_between_args(t_data *data, char **args_cmd, int y, t_bool do_i_return)
+{
+	if (args_cmd[y + 1] == NULL && do_i_return == B_TRUE)
+	{
+		if (write(STDOUT_FILENO, "\n", 1) == -1)
+			return (error_int(data, I_ECHO, strerror(errno), 1));
+	}
+	if (args_cmd[y + 1] != NULL)
+	{
+		if (write(STDOUT_FILENO, " ", 1) == -1)
+			return (error_int(data, I_ECHO, strerror(errno), 1));
+	}
+	return (0);
 }
 
 int	ft_echo(t_data *data, char **args_cmd)
@@ -58,22 +74,16 @@ int	ft_echo(t_data *data, char **args_cmd)
 
 	y = 1;
 	len = 0;
-	do_i_return = skip_option_n(data, args_cmd, &y);
+	if (args_cmd[1] == NULL)
+		return (echo_no_args(data));
+	do_i_return = skip_option_n(args_cmd, &y);
 	while (args_cmd[y] != NULL)
 	{
 		len = ft_strlen(args_cmd[y]);
 		if (write(STDOUT_FILENO, args_cmd[y], len) == -1)
 			return (error_int(data, I_ECHO, strerror(errno), 1));
-		if (args_cmd[y + 1] == NULL && do_i_return == B_TRUE)
-		{
-			if (write(STDOUT_FILENO, "\n", 1) == -1)
-				return (error_int(data, I_ECHO, strerror(errno), 1));
-		}
-		if (args_cmd[y + 1] != NULL)
-		{
-			if (write(STDOUT_FILENO, " ", 1) == -1)
-				error_int(data, I_ECHO, strerror(errno), 1);
-		}
+		if (print_between_args(data, args_cmd, y, do_i_return) == 1)
+			return (1);
 		y++;
 	}
 	return (0);
--- src_builtin/minishell_builtin.h
+++ src_builtin/minishell_builtin.h
@@ -46,8 +46,9 @@ int		export_no_args(t_data *data);
 
 // ft_echo
 int		ft_echo(t_data *data, char **args_cmd);
-t_bool	skip_option_n(t_data *data, char **args_cmd, int *y);
-void	echo_no_args(t_data *data);
+t_bool	skip_option_n(char **args_cmd, int *y);
+int	echo_no_args(t_data *data);
+int	print_between_args(t_data *data, char **args_cmd, int y, t_bool do_i_return);
 
 // ft_cd
 int		update_env_wd(t_data *data);
